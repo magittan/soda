e5912e38e903c5555b92061af1f3ed101bed4652
William Zheng <william.zheng@gmail.com>
2018-01-20 17:39:15 -0500

    Checking

--- patient.py
+++ patient.py
@@ -1,5 +1,18 @@
 class patient():
 
-    def __init__(name = "John Doe"):
+    def __init__(self, ID, name = "John Doe", **kwargs):
+        self.ID = ID
+        self.name = name
+        self.age = kwargs['age']
+        self.sex = kwargs['sex']
+
+class hospital():
+
+    def __init__(self, ID, **kwargs):
+        self.ID = ID
         self.name
-        self.
+        self.address
+        self.city
+        self.zipcode
+
+    def determineLatLong(self):
